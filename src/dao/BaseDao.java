d0eacb2c3237329108ba30e25b91116196cbbc8a
a1611 <a1611@LouisLiu>
2016-11-28 18:36:26 -0500

    fix BaseDao import error

--- src/dao/BaseDao.java
+++ src/dao/BaseDao.java
@@ -4,7 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import java.util.list;
+
+import java.util.List;
 
 @Repository("BaseDao")
 public class BaseDao {
