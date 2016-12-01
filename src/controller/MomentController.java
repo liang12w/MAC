694ef0fcc614371ed1e929b3ab4f75a977c5dfca
a1611 <a1611@LouisLiu>
2016-12-01 17:40:18 -0500

    watson update

--- src/controller/MomentController.java
+++ src/controller/MomentController.java
@@ -48,26 +48,21 @@ public class MomentController {
 		service.setApiKey("cf12a4426504285e2a30fcebd1933f4c133141a7");
 
 		Map<String, Object> map = new HashMap<String, Object>();
-	    map.put(AlchemyLanguage.TEXT, "The technology definitely has its limits. It can't produce videos that go further than 1.5 seconds into the future, and the results aren't mind-blowingly realistic: it isn't aware that objects are still there when they move, and tends to exaggerate their sizes. However, it's good enough to predict relatively complicated scenes like waves on the beach, or people walking on grass. If CSAIL can extend predictions and make them more realistic, though, it could have a far-reaching impact. Self-driving cars could predict where vehicles and pedestrians are going, while security cameras could spot mismatches in footage based on what they expect to see. It could also be used for relatively everyday tasks like adding animation to still images or compressing videos (since you wouldn't need every frame). And regardless of circumstances, predicting the future can help AI understand what's going on right now -- this could help with just about any instance where computer vision is important.");
+	    map.put(AlchemyLanguage.TEXT, content);
 	    
 	    Entities entities = service.getEntities(map).execute();
-	    System.out.println("Entities: " + entities);
+//	    System.out.println("Entities: " + entities);
 	    JSONArray array = JSON.parseArray(entities.getEntities().toString());
 	    JSONObject entity = array.getJSONObject(0);
 	    String keyword = entity.getString("text");
 	    String url = genGif(keyword);
-	    String type = entity.getString("type");
-//	    System.out.println("text is "+keyword+" type is "+ type);
-//	    return url;
+//	    String type = entity.getString("type");
 	    map.remove(AlchemyLanguage.TEXT);
 		map.put("errorCode", 0);
 		map.put("url", url);
 		return map;
 	}
 	public String genGif(String keyword){
-//		JSONArray jsonArray = (JSONArray) JSON.parse(HttpUtils.getHttpResult("http://api.giphy.com/v1/gifs/search?q="+keyword+"&api_key=dc6zaTOxFJmzC"));
-//		JSONObject entity = jsonArray.getJSONObject(0);
-//	    String url = entity.getString("text");
 		String content = "http://api.giphy.com/v1/gifs/search?q="+keyword+"&api_key=dc6zaTOxFJmzC";
 		String text = HttpUtils.getHttpResult(content);
 		JSONObject json = JSON.parseObject(text);
