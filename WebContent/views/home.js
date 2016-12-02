e5854478dfe1630f898dd664b42b401b946938b7
a1611 <a1611@LouisLiu>
2016-12-01 22:24:51 -0500

    show All Moments desc

--- WebContent/views/home.js
+++ WebContent/views/home.js
@@ -18,7 +18,7 @@ function init(){
 		data:{'sid':$.cookie('sid')},
 		success:function(data){
 			if(data.errorCode== 0){
-				generateInfo(data.moments);
+				generateInfo(data.list);
 			}else if (data.errorCode==-1) {
 				alert(data.errorMsg);
 				window.location.href = '../login.html';
@@ -34,11 +34,16 @@ $(init)
 
 function generateInfo(data){
 	var content = undefined;
+	// console.info(data[0]["motId"]);
+	// console.info(data[0]["userInfo"]["usrName"]);
+	// var time = data[0]["motSentTime"];
+	// var date = Date(data[0]["motSentTime"]);
+	// console.info(Date(data[0]["motSentTime"]));
 	for (var i = 0; i < data.length; i++) {
-		cont += "<article id='"+data.motId+"'><div class='heading'><h2>"+data[i].motContent+"</h2></div>"+
-					"<div class='content'><img src='"+data[i].motGifUri+"' width='200px' height='200px' /></div>"+
-					"<div class='info'><p>By "+data[i].userInfo.usrName+" on "+data[i].motSentTime+
-					" - <a href='javascript:getComments('"+data.motId+"');' >"+data[i].motCommentNum+" Commnets</a></p>"+
+		content += "<article id='"+data[i]["motId"]+"'><div class='heading'><h2>"+data[i]["motContent"]+"</h2></div>"+
+					"<div class='content'><img src='"+data[i]["motGifUri"]+"' width='200px' height='200px' /></div>"+
+					"<div class='info'><p>By "+data[i]["userInfo"]["usrName"]+" on "+Date(data[0]["motSentTime"])+
+					" - <a href='javascript:getComments('"+data[i]["motId"]+"');' >"+data[i]["motCommentNum"]+" Commnets</a></p>"+
 					"</div></article>";
 	};
 	$(".col-md-6").append(content);
